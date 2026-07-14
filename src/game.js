065a5255f1816ffda404c32416e86ad1cf7f7f63
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2026-07-14 13:25:45 +0200

    placeFleet()

--- src/game.js
+++ src/game.js
@@ -8,27 +8,44 @@ import Ship from "./Ship.js";
 const player = new Player("human");
 const computer = new Player("computer");
 
+ 
+function placeRandomShip(gameboard, length) {
+  let placed = false;
+
+  while (!placed) {
+    const row = Math.floor(Math.random() * 10);
+    const col = Math.floor(Math.random() * (11 - length));
+
+    const coordinates = [];
+
+    for (let i = 0; i < length; i++) {
+      coordinates.push([row, col + i]);
+    }
+
+    const overlaps = gameboard.ships.some((placedShip) =>
+      placedShip.coordinates.some(([r, c]) =>
+        coordinates.some(([newR, newC]) => r === newR && c === newC)
+      )
+    );
+
+    if (!overlaps) {
+      gameboard.placeShip(new Ship(length), coordinates);
+      placed = true;
+    }
+  }
+}
+
+function placeFleet(gameboard) {
+  const ships = [5, 4, 3, 3, 2];
+
+  ships.forEach((length) => {
+    placeRandomShip(gameboard, length);
+  });
+}
 
 
-
-// trwałe rozmieszczenie statków na planszy
-player.gameboard.placeShip(
-  new Ship(3),
-  [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-  ]
-);
-
-computer.gameboard.placeShip(
-  new Ship(3),
-  [
-    [5, 5],
-    [5, 6],
-    [5, 7],
-  ]
-);
+ placeFleet(player.gameboard);
+placeFleet(computer.gameboard);
 
 
 
