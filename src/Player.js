c6f6d1b21d1b724b0b3912b94bb05d6807b96bec
PawelBelczyk <227667461+PawelBelczyk@users.noreply.github.com.>
2026-07-14 15:22:54 +0200

    ai hunter 3.0

--- src/Player.js
+++ src/Player.js
@@ -6,6 +6,7 @@ export default class Player {
         this.gameboard = new Gameboard();
         this.moves = [];
         this.successfulHits = [];
+        this.hitDirection = null;
     }
 
 
@@ -34,12 +35,73 @@ export default class Player {
 
     getSmartMove() {
 
-        const directions = [
+        const direction =  this.getHitDirection();
+
+          if (direction) {
+
+              const hits = this.successfulHits;
+
+              const sortedHits = [...hits].sort(
+                  (a,b) =>
+                      direction === "horizontal"
+                      ? a[1] - b[1]
+                      : a[0] - b[0]
+              );
+
+
+              const first = sortedHits[0];
+              const last = sortedHits[sortedHits.length - 1];
+
+
+              const targets =
+                  direction === "horizontal"
+                  ? [
+                      [first[0], first[1]-1],
+                      [last[0], last[1]+1]
+                    ]
+                  : [
+                      [first[0]-1, first[1]],
+                      [last[0]+1, last[1]]
+                    ];
+
+
+              const validTargets = targets.filter(
+                  ([row,col]) =>
+                      row >= 0 &&
+                      row < 10 &&
+                      col >= 0 &&
+                      col < 10 &&
+                      !this.moves.some(
+                          ([r,c]) =>
+                              r === row &&
+                              c === col
+                      )
+              );
+
+
+              if(validTargets.length > 0) {
+
+                  const move =
+                      validTargets[
+                          Math.floor(
+                              Math.random() *
+                              validTargets.length
+                          )
+                      ];
+
+                  this.moves.push(move);
+
+                  return move;
+              }
+
+          }
+
+                  const directions = [
             [-1, 0],
             [1, 0],
             [0, -1],
             [0, 1]
-        ];
+        ];  
 
 
         while (this.successfulHits.length > 0) {
@@ -94,5 +156,30 @@ export default class Player {
 
     clearHits() {
     this.successfulHits = [];
+    this.hitDirection = null;
   }
+
+        getHitDirection() {
+
+          if (this.successfulHits.length < 2) {
+              return null;
+          }
+
+
+          const first = this.successfulHits[0];
+          const second = this.successfulHits[1];
+
+
+          if (first[0] === second[0]) {
+              return "horizontal";
+          }
+
+
+          if (first[1] === second[1]) {
+              return "vertical";
+          }
+
+
+          return null;
+      }
 }
